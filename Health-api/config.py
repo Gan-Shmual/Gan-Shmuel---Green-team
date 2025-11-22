b99764f001a29afed898285a839ff14e27fbd606
Denis <lazerman743@gmail.com>
2025-11-22 02:25:25 +0200

    Deleted other branches files (just weight will be here)

--- Health-api/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_USER = os.getenv("DB_USER", "weight_user")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "weight_pass")
-DB_NAME = os.getenv("DB_NAME", "weight")
