f41b0d0224a847e5ebdd1d7a0d35d473b01528f7
Denis <lazerman743@gmail.com>
2025-11-23 14:05:27 +0200

    Changed directories

--- README.md
+++
@@ -1,1 +0,0 @@
-<img width="964" height="772" alt="image" src="https://github.com/user-attachments/assets/5b92d01a-94ee-4bee-9a59-20c50ae48a21" />
--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DB_HOST = os.getenv("DB_HOST", "weight-db")
-DB_USER = os.getenv("DB_USER", "weight_user")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "weight_pass")
-DB_NAME = os.getenv("DB_NAME", "weight")
-
