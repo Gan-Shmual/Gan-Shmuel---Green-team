80758dc67139df0c20f749d268afd6f149fff8c2
Ahmad <ahmad.zbedaat1208@gmail.com>
2025-11-24 21:04:48 +0200

    DevOPS: add billing to docker-compose.tests

--- tests/test_service.py
+++ tests/test_service.py
@@ -3,13 +3,13 @@ import time
 import pytest
 
 
-BASE = "http://weight-app:5000"
+WEIGHT_BASE = "http://weight-app:5000"
 #helper func to wait for the service(db included)
-def wait_for_service(timeout=30):
+def wait_for_weight_service(timeout=30):
     start = time.time()
     while True:
         try:
-            r = requests.get(f"{BASE}/health", timeout=2)
+            r = requests.get(f"{WEIGHT_BASE}/health", timeout=2)
             if r.status_code == 200:
                 return
         except requests.exceptions.RequestException:
@@ -22,8 +22,33 @@ def wait_for_service(timeout=30):
         time.sleep(1)
 
 def test_weight_health():
-    wait_for_service()
-    r = requests.get(f"{BASE}/health")
+    wait_for_weight_service()
+    r = requests.get(f"{WEIGHT_BASE}/health")
     assert r.status_code == 200
+
+BILLING_BASE = "http://billing-service:8000"
+
+def wait_for_billing_service(timeout=30):
+    start = time.time()
+    while True:
+        try:
+            r = requests.get(f"{BILLING_BASE}/health", timeout=2)
+            if r.status_code == 200:
+                return
+        except requests.exceptions.RequestException:
+            
+            pass
+
+        if time.time() - start > timeout:
+            raise TimeoutError("Billing service did not become healthy in time")
+
+        time.sleep(1)
+
+def test_billing_health():
+    wait_for_billing_service()
+    r = requests.get(f"{BILLING_BASE}/health")
+    assert r.status_code == 200
+
+
 
 
